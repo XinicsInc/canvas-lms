ce9119bafc405e78052c5c1cce05fd8cacc96dd3
XinicsBori <qhgus0404@xinics.com>
2026-02-20 18:45:00 +0900

    LXCCOP-759 강의계획서 길이 검사 조건 완화
    - 500 kb -> 1 mb

--- app/models/course.rb
+++ app/models/course.rb
@@ -247,7 +247,7 @@ class Course < ActiveRecord::Base
   validate :validate_default_view
   validates :sis_source_id, uniqueness: {scope: :root_account}, allow_nil: true
   validates_presence_of :account_id, :root_account_id, :enrollment_term_id, :workflow_state
-  validates_length_of :syllabus_body, :maximum => maximum_long_text_length, :allow_nil => true, :allow_blank => true
+  validates_length_of :syllabus_body, :maximum => 1.megabyte-1, :allow_nil => true, :allow_blank => true
   validates_length_of :name, :maximum => maximum_string_length, :allow_nil => true, :allow_blank => true
   validates_length_of :sis_source_id, :maximum => maximum_string_length, :allow_nil => true, :allow_blank => false
   validates_length_of :course_code, :maximum => maximum_string_length, :allow_nil => true, :allow_blank => true
