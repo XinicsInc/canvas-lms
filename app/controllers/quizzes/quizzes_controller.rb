98451e37ef675412908313c1aa4d91357b4050af
kimth <kimth@xinics.com>
2026-02-27 17:57:10 +0900

    LXCCUP-196 문제 펼쳐보기 제한을 25에서 100으로 상향
    (cherry picked from commit 3baf24b8e4169d9bbbe334c81c3060777c9b96d3)

--- app/controllers/quizzes/quizzes_controller.rb
+++ app/controllers/quizzes/quizzes_controller.rb
@@ -52,7 +52,8 @@ class Quizzes::QuizzesController < ApplicationController
   after_action :lock_results, only: [ :show, :submission_html ]
   # The number of questions that can display "details". After this number, the "Show details" option is disabled
   # and the data is not even loaded.
-  QUIZ_QUESTIONS_DETAIL_LIMIT = 25
+  # [LXCCUP-196] 문제 펼쳐보기 제한을 25에서 100으로 상향
+  QUIZ_QUESTIONS_DETAIL_LIMIT = 100
   QUIZ_MAX_COMBINATION_COUNT = 200
 
   QUIZ_TYPE_ASSIGNMENT = 'assignment'
